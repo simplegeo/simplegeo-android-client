72a26deb5112b2c81354b536c8c4e32ad656d8d5
dsmith <derek@simplegeo.com>
2010-05-27 15:37:30 -0600

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,14 @@
+SGClient v1.0.0
+================================================================================
+
+ABSTRACT:
+--------------------------------------------------------------------------------
+
+A thread-safe, Java HttpClient used to interface with the SimpleGeo API. Both
+synchronous and asynchronous calls are provided.
+
+CHANGES FROM PREVIOUS VERSIONS:
+--------------------------------------------------------------------------------
+Version 1.0.0
+
+### Copyright (C) 2010 SimpleGeo Inc. All rights reserved.
---
+++ tests/src/com/simplegeo/client/model/LayerTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2010 SimpleGeo. All rights reserved.
+ */
+package com.simplegeo.client.model;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+
+import com.simplegeo.client.test.TestEnvironment;
+import com.simplegeo.client.service.LocationService;
+import com.simplegeo.client.test.ModelHelperTest;
+
+
+/**
+ * @author Derek Smith
+ *
+ */
+public class LayerTest extends ModelHelperTest {
+
+	private Layer testingLayer;
+	
+	public void setUp() throws Exception {
+		
+		LocationService.getInstance().getHttpClient().setToken(TestEnvironment.getKey(), TestEnvironment.getSecret());
+		testingLayer = new Layer(TestEnvironment.getLayer());
+		
+	}
+
+	public void tearDown() {
+		
+		LocationService locationService = LocationService.getInstance();
+		locationService.futureTask = false;
+		
+		List<IRecord> records = testingLayer.getRecords();
+		
+		try {
+			
+			for(IRecord record : records)
+				locationService.delete(record);
+			
+		} catch (ClientProtocolException e) {
+			assertTrue(e.getMessage(), false);
+		} catch (IOException e) {
+			assertTrue(e.getMessage(), false);	
+		}
+	}
+
+	public void testLayerRetrieval() throws Exception {
+		
+		
+		testingLayer.add(getRandomDefaultRecordList(50));
+		try {
+			
+			testingLayer.update();
+			
+		} catch (ClientProtocolException e) {
+			assertTrue(e.getMessage(), false);
+		} catch (IOException e) {
+			assertTrue(e.getMessage(), false);
+		}
+		
+		
+	}
+}
