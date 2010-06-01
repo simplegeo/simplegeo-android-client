8aad91f3d2c3de3cc4a33729f5e35a32c00b81fd
dsmith <derek@simplegeo.com>
2010-06-01 16:20:15 -0600

    Removed the LocationHandler because we no longer require a MessageQueue when dealing with a single instance of the LocationService.

--- src/com/simplegeo/android/service/LocationService.java
+++ src/com/simplegeo/android/service/LocationService.java
@@ -1,12 +1,9 @@
 package com.simplegeo.android.service;
 
-import com.simplegeo.android.service.handlers.LocationHandler;
+import java.util.List;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.location.GpsStatus.Listener;
-import android.location.GpsStatus.NmeaListener;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -16,22 +13,32 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Messenger;
 
-public class LocationService extends Service {
+public class LocationService extends Service implements LocationListener {
 	
-	private Criteria locationCriteria;
-			
-	final Messenger messanger = new Messenger(new LocationHandler());
+	public static final String MIN_TIME = "minimum_time";
+	public static final String MIN_DISTANCE = "minimun_distance";
+	
+	private long minTime;
+	private float minDistance;
+
+    public class LocationBinder extends Binder {
+        LocationService getService() {
+            return LocationService.this;
+        }
+    }
+    
+    private final LocationBinder locationBinder = new LocationBinder();
 	
 	@Override
 	public IBinder onBind(Intent intent) {
-		return messanger.getBinder();
+		minTime = intent.getLongExtra(MIN_TIME, 0);
+		minDistance = intent.getFloatExtra(MIN_DISTANCE, 0.0f);		
+		return locationBinder;
 	}
 	
     @Override
     public void onCreate() {
-		this.locationCriteria = generateCriteria();
-		LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-		
+		updateProviders();
     }	
     
     @Override
@@ -39,18 +46,40 @@ public class LocationService extends Service {
     	
     }
     
-	private void startListening() {
-		
-		
-	}
-	
-	private void stopListening() {
-		
-	}
+    public void updateProviders() {
+    	updateProviders(null);
+    }
+    public void updateProviders(Criteria criteria) {
+    	if(criteria == null)
+    		criteria = generateCriteria();
+    	
+		LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+		List<String> providerNames = locationManager.getProviders(criteria, true);
+		for(String providerName : providerNames)
+			locationManager.requestLocationUpdates(providerName, minTime, minDistance, this);
+    }
 	
 	private Criteria generateCriteria() {
 		Criteria criteria = new Criteria();
 		
+		// Do some setup of the criteria.
+		
 		return criteria;
+	}
+
+	public void onLocationChanged(Location location) {
+		// Update each handler
+	}
+
+	public void onProviderDisabled(String provider) {
+		// Don't really care...
+	}
+
+	public void onProviderEnabled(String provider) {
+		// Don't really care (or add as a listener)
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// Don't really care
 	}
 }
--- src/com/simplegeo/android/service/handlers/LocationHandler.java
+++
@@ -1,7 +0,0 @@
-package com.simplegeo.android.service.handlers;
-
-import android.os.Handler;
-
-public class LocationHandler extends Handler {
-
-}
