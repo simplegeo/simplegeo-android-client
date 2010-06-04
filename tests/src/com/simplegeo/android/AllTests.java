53b81a855e02b3f4bf3a2dfd27492ff7554075c2
dsmith <derek@simplegeo.com>
2010-06-04 07:41:00 -0700

    Added the test loader class

--- tests/src/com/simplegeo/android/AllTests.java
+++ tests/src/com/simplegeo/android/AllTests.java
@@ -26,7 +26,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.simplegeo.client;
+package com.simplegeo.android;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
